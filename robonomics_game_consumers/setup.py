01a8af4ce5785baab88fa99a7c791afd44c6dda3
Alisher Khassanov <a.khssnv@gmail.com>
2018-02-11 14:11:55 +0400

    Package rename to established format

--- robonomics_game_consumers/setup.py
+++ robonomics_game_consumers/setup.py
@@ -6,6 +6,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['robonomics_consumers'],
+    packages=['robonomics_game_consumers'],
     package_dir={'': 'src'})
 setup(**setup_args)
